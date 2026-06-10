dca8dfd768afec781b1225fe5ae9367c5f35a7b2
kirillepishev <kirill.epishev@gmail.com>
2026-06-10 16:57:16 +0300

    prepare pythonanywhere

--- config/settings.py
+++ config/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'megateks_db',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
